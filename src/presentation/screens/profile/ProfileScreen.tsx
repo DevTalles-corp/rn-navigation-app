c25c6650a020177cc67bb7e87bff047a168c467e
Fernando Herrera <fernando.herrera85@gmail.com>
2023-10-30 13:19:03 -0400

    Toggle implementado

--- src/presentation/screens/profile/ProfileScreen.tsx
+++ src/presentation/screens/profile/ProfileScreen.tsx
@@ -1,9 +1,28 @@
 import { Text, View } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { PrimaryButton } from '../../components/shared/PrimaryButton';
+import { DrawerActions, useNavigation } from '@react-navigation/native';
 
 export const ProfileScreen = () => {
+
+
+  const { top } = useSafeAreaInsets();
+  const navigation = useNavigation();
+
+
   return (
-    <View>
-      <Text>ProfileScreen</Text>
+    <View style={{
+      flex: 1, 
+      paddingHorizontal: 20,
+      marginTop: top
+    }}>
+      <Text style={{ marginBottom: 10 }}>ProfileScreen</Text>
+
+
+      <PrimaryButton 
+        onPress={ () => navigation.dispatch( DrawerActions.toggleDrawer ) }
+        label="Abrir menú"
+      />
     </View>
   )
 }
