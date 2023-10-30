8f7d147f8080d5ccc43bdd3f9dd7b2204bdc4928
Fernando Herrera <fernando.herrera85@gmail.com>
2023-10-30 11:55:26 -0400

    PopToTop

--- src/presentation/screens/settings/SettingsScreen.tsx
+++ src/presentation/screens/settings/SettingsScreen.tsx
@@ -1,9 +1,30 @@
 import { Text, View } from 'react-native';
+import { globalStyles } from '../../theme/theme';
+import { PrimaryButton } from '../../components/shared/PrimaryButton';
+import { StackActions, useNavigation } from '@react-navigation/native';
 
 export const SettingsScreen = () => {
+
+  const navigator = useNavigation();
+
+
   return (
-    <View>
-      <Text>SettingsScreen</Text>
+    <View style={ globalStyles.container }>
+      
+      <Text style={{ marginBottom: 10 }}>Settings Screen</Text>
+
+      <PrimaryButton 
+        label="Regresar"
+        onPress={ () =>  navigator.goBack() } 
+      />
+
+      <PrimaryButton 
+        label="Regresar al Home"
+        onPress={ () =>  navigator.dispatch( StackActions.popToTop() ) } 
+      />
+
+
+
     </View>
   )
 }
